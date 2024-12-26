cc5b81fdaf96a6cf56ec115107a79784bc283295
yoon-chaejin <kevin3st@naver.com>
2024-12-27 04:41:31 +0900

    chore : MySQL 설정

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO Lecture (id, title, registration_count, lecture_by, lecture_at)
-VALUES (1, '강의1', '0', '1', PARSEDATETIME('202412281300','yyyyMMddHHmm'));
-INSERT INTO Lecture (id, title, registration_count, lecture_by, lecture_at)
-VALUES (2, '강의2', '0', '1', PARSEDATETIME('202412281500','yyyyMMddHHmm'));
-INSERT INTO Lecture (id, title, registration_count, lecture_by, lecture_at)
-VALUES (3, '강의3', '0', '1', PARSEDATETIME('202412290500','yyyyMMddHHmm'));
+INSERT INTO lecture (id, title, registration_count, lecture_by, lecture_at)
+VALUES (1, '강의1', '0', '1', STR_TO_DATE('202412281300','%Y%m%d%H%i'));
+INSERT INTO lecture (id, title, registration_count, lecture_by, lecture_at)
+VALUES (2, '강의2', '0', '1', STR_TO_DATE('202412281500','%Y%m%d%H%i'));
+INSERT INTO lecture (id, title, registration_count, lecture_by, lecture_at)
+VALUES (3, '강의3', '0', '1', STR_TO_DATE('202412290500','%Y%m%d%H%i'));
